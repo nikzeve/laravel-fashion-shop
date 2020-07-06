e5cda65acae2891fef4af6a60cd53f02c72b579c
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-07-06 18:50:09 +0200

    push intermedia

--- app/Http/Controllers/DressController.php
+++ app/Http/Controllers/DressController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Dress;
+
 class DressController extends Controller
 {
     public function index() {
         $all_clothes = Dress::all();
+        dd($all_clothes);
     }
 }
